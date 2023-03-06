3858750f3cfc983c7599ac41ca0b596b7e13c5dc
loammarwork <lo.ammar.work@gmail.com>
2023-03-06 20:44:32 +0700

    first commit

---
+++ gateways/search.ts
@@ -0,0 +1,104 @@
+import * as z from "zod";
+import { ZodError } from "zod";
+import { Response, NextFunction } from "express";
+import { HttpStatusCode } from "axios";
+import { ResponseError, ResponseSuccess } from "../helper/ResponseTranslator";
+
+const SearchGatewayRequest = z.object({
+    params: z.any({}),
+    headers: z.any({}),
+    body: z.object({
+        keyword: z.string(),
+        from: z.string(),
+        to: z.string(),
+        language: z.string(),
+        pagination: z.object({
+            itemsPerPage: z.number(),
+            page: z.number(),
+        }),
+    }),
+    query: z.any({}),
+});
+
+export type SearchGatewayRequestType = z.infer<typeof SearchGatewayRequest>;
+
+export interface SearchGatewayAPIStandardResponse {
+    success: boolean;
+    identifier: string;
+    data: Daum[];
+    pagination: Pagination;
+}
+
+export interface Daum {
+    activityId: string;
+    activityName: string;
+    activityType: string;
+    location: Location;
+    segments: Segment[];
+    currency: string;
+    targetMarket: any[];
+    amountFrom: AmountFrom;
+    images: Image[];
+    freeCancellation: boolean;
+}
+
+export interface Location {
+    country: Country;
+}
+
+export interface Country {
+    name: string;
+    destination: Destination;
+}
+
+export interface Destination {
+    name: string;
+}
+
+export interface Segment {
+    segmentName: string;
+    segmentValues: string[];
+}
+
+export interface AmountFrom {
+    amount: number;
+    nettAmount: number;
+    boxOfficeAmount: number;
+}
+
+export interface Image {
+    imageType: string;
+    urls: Url[];
+}
+
+export interface Url {
+    imageSize: string;
+    resource: string;
+}
+
+export interface Pagination {
+    itemsPerPage: number;
+    page: number;
+    totalItems: number;
+    itemsCount: number;
+}
+
+export function ValidateSearchGateway(
+    req: SearchGatewayRequestType,
+    res: Response<ResponseSuccess<SearchGatewayAPIStandardResponse> | ResponseError>,
+    next: NextFunction
+) {
+    try {
+        req = SearchGatewayRequest.parse(req);
+        next();
+    } catch (error: unknown) {
+        if (error instanceof Error)
+            return res
+                .status(HttpStatusCode.BadRequest)
+                .json(new ResponseError({ code: HttpStatusCode.BadRequest, message: error.message }));
+        if (error instanceof ZodError)
+            return res
+                .status(HttpStatusCode.BadRequest)
+                .json(new ResponseError({ code: HttpStatusCode.BadRequest, message: error.message }));
+    }
+}
---
+++ helper/ResponseTranslator.ts
@@ -0,0 +1,20 @@
+class ResponseError {
+    public success: boolean;
+    public error: { code: number; message: string };
+    constructor(error: { code: number; message: string }) {
+        this.success = false;
+        this.error = error;
+    }
+}
+
+class ResponseSuccess<T> {
+    public success: boolean;
+    public data: T;
+
+    constructor(data: T) {
+        this.success = true;
+        this.data = data;
+    }
+}
+
+export { ResponseError, ResponseSuccess };
